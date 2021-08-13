6cfb1ec6341854c9df2bb5c16f378df4b5719431
Dorau Patryk <patryk.dorau@cosmogroup.pl>
2021-08-13 08:39:06 +0200

    apka-vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <Header :showAddTask = "showAddTask" @show-form="showTask" title = 'Task Tracker'/>
+      <div v-show="showAddTask">
+        <addTask @add-task="addingTask"/>
+      </div>
+      <Tasks @toggle-reminder = "toggleReminder" @delete-task="deleteTask" v-bind:tasks = "tasks"/>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import addTask from './components/addTask'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks,
+    addTask,
+    
+  },
+  data() {
+    return{
+      tasks:[],
+      showAddTask:false
+      }
+  },
+  methods: {
+    deleteTask(id){
+      if(confirm('Are you sure?')){
+        this.tasks = this.tasks.filter((task)=> task.id !== id)
+      }
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map((task)=>task.id === id ? {...task, reminder: !task.reminder} : task)
+    },
+    addingTask(newTask){
+      this.tasks = [...this.tasks, newTask]
+    },
+    showTask(){
+      this.showAddTask = !this.showAddTask
+    }
+  },
+  created(){
+    this.tasks = [
+      {
+        id:1,
+        text: 'pierwsze',
+        day:"iabdai",
+        reminder:true,
+      },
+      {
+        id:2,
+        text:"drugie",
+        day:"dadafae",
+        reminder:false,
+      }
+    ]
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @show-form="$emit('show-form')" :color = "showAddTask ? 'red' : 'green'" :text = "showAddTask ? 'Close':'Add Task'"/>
+    </header>
+</template>
+
+<script>
+import Button from './button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask:Boolean,
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
